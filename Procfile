60547c5e2c49e88b9dd698c266fae75e35013e66
Fielding Johnston <fielding@justfielding.com>
2022-06-14 15:12:11 -0500

    Update for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: POOL_SIZE=2 mix ecto.migrate
-web: mix run --no-halt
+web: _build/prod/rel/whos_in_bot/bin/whos_in_bot start
--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :nadia, token: (System.get_env("BOT_TOKEN") || "")
 
 config :whos_in_bot, ecto_repos: [WhosInBot.Repo]
-config :whos_in_bot, port: 5000
+config :whos_in_bot, port: 4000
 
 import_config "#{Mix.env}.exs"
