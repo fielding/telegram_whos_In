6bc9712ba4eca98006612caa7c5a42e48169c14d
Fielding Johnston <fielding@justfielding.com>
2022-06-14 15:48:47 -0500

    Use DATABASE_URL instead of individual config env vars
    this way it will stay updated when heroku does db maint
    
    Add forgotten comma
    
    fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -4,11 +4,7 @@ config :logger, level: :warn
 
 config :whos_in_bot,
        WhosInBot.Repo,
-       username: System.get_env("DB_USER", "postgres"),
-       password: System.get_env("DB_PASSWORD", "postgres"),
-       database: System.get_env("DB_NAME", "whos_in_bot"),
-       hostname: System.get_env("DB_HOST", "localhost"),
-       port: System.get_env("DB_PORT", "5432") |> String.to_integer(),
+       url: System.get_env("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/whos_in_bot"),
        ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom()
 
 if File.exists?("prod.secret.exs") do
--- config/releases.exs
+++ config/releases.exs
@@ -4,10 +4,7 @@ config :logger, level: System.get_env("LOG_LEVEL", "info") |> String.to_atom()
 
 config :whos_in_bot,
        WhosInBot.Repo,
-       username: System.fetch_env!("DB_USER"),
-       password: System.fetch_env!("DB_PASSWORD"),
-       database: System.fetch_env!("DB_NAME"),
-       hostname: System.fetch_env!("DB_HOST"),
+       url: System.fetch_env!("DATABASE_URL"),
        port: System.get_env("DB_PORT", "5432") |> String.to_integer(),
        ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom(),
        pool_size: System.get_env("DB_POOL_SIZE", "10") |> String.to_integer()
